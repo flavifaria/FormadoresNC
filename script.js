50ee0d8b57e822d26d91271e4b4e96f10df0a6cc
Flavio Faria <135338397+flavifaria@users.noreply.github.com>
2025-07-29 20:22:41 -0300

    Criação de obejto em DOM

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+
 function inserirNome(){
     let nome = prompt("Digite seu nome:");
     let elemento = document.querySelector("#nome");
     elemento.innerHTML = nome;
 }
 
+// Manipulação de DOM
+// O DOM (Document Object Model) é uma interface de programação para documentos HTML e XML.
 inserirNome();
 let linguagens = ["Javascript", "Python", "C"];
-const item = document.querySelector("#lista");
+const item = document.querySelector(".lista");
 item.innerHTML = linguagens[0] + ", " + linguagens[1] + ", " + linguagens[2];
+
+// Objetos em JavaScript
+// Objetos são estruturas que permitem agrupar dados e funcionalidades relacionadas.
+let aluno1 = {
+  nome: "Gabriel",
+  idade: 13,
+  anoLetivo: "2ºsérie",
+  materiasFavoritas: ["Geografia","Programação","Arte"]
+}
+
+// Acessando propriedades do objeto
+console.log(aluno1.nome); // “Gabriel”
+console.log(aluno1.idade); // 13
+console.log(aluno1.materiasFavoritas);
+// ["Geografia","Programação","Arte"]
+console.log(aluno1.materiasFavoritas[1]); // "Programação"
+
+const objeto = document.querySelector("#aluno");
+objeto.textContent = aluno1.nome;
