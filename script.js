7867c7da34cedbd992f93ccd5ee4bcca1a6c5d17
Flavio Faria <135338397+flavifaria@users.noreply.github.com>
2025-07-29 21:00:35 -0300

    DOM Update

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
-// Manipulação de variáveis
-function inserirNome() {
-  let nome = prompt("Digite seu nome:");
-  let elemento = document.querySelector("#nome");
-  elemento.innerHTML = nome;
+function inserirNome()  {
+    let nomeUsuario = prompt("Qual o seu nome?");
+    let elemento = document.querySelector("#nome-usuario");
+    elemento.textContent = nomeUsuario;
 }
-
-// Manipulação de DOM
 inserirNome();
-let linguagens = ["Javascript", "Python", "C"];
-const item = document.querySelector(".lista");
-item.innerHTML = ""; // Limpa antes de adicionar
-linguagens.forEach(lang => {
-  item.innerHTML += `<li>${lang}</li>`;
-});
 
-// Objetos em JavaScript
+const linguagens = ["Javascript", "Python", "C"];
+// indice      
+linguagens[2] = "HTML";
+console.log(linguagens[2]);       
+const item = document.querySelector("#lista");
+item.textContent = linguagens[1];
+item.textContent = linguagens[2];
+item.textContent = linguagens[0];
+
 let aluno1 = {
   nome: "Gabriel",
   idade: 13,
   anoLetivo: "2ºsérie",
-  materiasFavoritas: ["Geografia", "Programação", "Arte"]
+  materiasFavoritas: ["Geografia","Programação","Arte"]
 }
 
-// Acessando propriedades do objeto
-console.log(aluno1.nome);
-console.log(aluno1.idade);
+console.log(aluno1.nome); // “Gabriel”
+console.log(aluno1.idade); // 13
 console.log(aluno1.materiasFavoritas);
+// ["Geografia","Programação","Arte"]
 console.log(aluno1.materiasFavoritas[1]);
 
 const objeto = document.querySelector("#aluno");
-objeto.innerHTML = `
-  Nome: ${aluno1.nome}<br>
-  Idade: ${aluno1.idade}<br>
-  Matérias Favoritas:
-  <ul>
-    ${aluno1.materiasFavoritas.map(m => `<li>${m}</li>`).join("")}
-  </ul>
-`;
+objeto.textContent = aluno1.nome;
+objeto.textContent = aluno1.idade;
